3b5e9895a49dd7d482f4cb8d1b1921b02af090c3
vmalaviya <varunmalaviya7@gmail.com>
2023-09-14 15:11:35 -0400

    setting up a basic redux store for tracking state values with actions and reducers

---
+++ src/state/store.ts
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+/* initial state for cart */
+const initialState: { isCartOpen: boolean, cart: { id: string, count: number }[], items: [] } = {
+    isCartOpen: false,
+    cart: [],
+    items: []
+}
+
+/* CART SLICE for updating state */
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        setItems: (state,action) => {
+            state.items = action.payload
+        },
+        /* reducer for adding items to cart */
+        addToCart: (state,action) => {
+            state.cart = [ ...state.cart, action.payload.item ]
+        },
+        /* reducer for removing items for cart */
+        removeFromCart: (state,action) => {
+            state.cart = state.cart.filter((item) => item.id !== action.payload.id)
+        },
+        /* reducer for increasing item count to cart */
+        increaseCount: (state,action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.item){
+                    item.count++
+                }
+                return item
+            })
+        },
+        /* reducer for decreasing item count to cart */
+        decreaseCount: (state,action) => {
+            state.cart = state.cart.map((item) => {
+                if (item.id === action.payload.item && item.count > 1){
+                    item.count--
+                }
+                return item
+            })
+        },
+        /* reducer for opening/closing cart*/
+        setIsCartOpen: (state,action) => {
+            state.isCartOpen = !state.isCartOpen
+        }
+    }
+})
+
+export const {
+    setItems,
+    addToCart,
+    removeFromCart,
+    increaseCount,
+    decreaseCount,
+    setIsCartOpen
+} = cartSlice.actions
+
+export default cartSlice.reducer
