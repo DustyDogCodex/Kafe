2adc627caae1b200414e0294a24cd2c8300db948
vmalaviya <varunmalaviya7@gmail.com>
2023-09-16 14:29:15 -0400

    added a .env file, setting up mongodb thru mongoose

---
+++ src/utils/mongodb.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+export async function connectMongo () {
+    try {
+        mongoose.connect(process.env.MONGO_URL!)
+        const connection = mongoose.connection
+
+        /* success message in console when connection is established */
+        connection.on('connected', () => {
+            console.log('Successfully connected to database!')
+        })
+
+        //for handling connection errors when connecting to db
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running' + err)
+            process.exit()
+        })
+    } catch(err) {
+        console.log(err)
+    }
+}
